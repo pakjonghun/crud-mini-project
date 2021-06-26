18b2419da8bcd27660f27b7f75a07a5f39fb473e
정훈박 <fireking5997@gmail.com>
2021-06-26 23:04:38 +0900

    delete기능 완성

--- src/controller/detailController.js
+++ src/controller/detailController.js
@@ -1,4 +1,5 @@
 import Comment from "../schemas/comments";
+import bcrypt from "bcrypt";
 
 export const detail = async (req, res) => {
   try {
@@ -17,4 +18,27 @@ export const detail = async (req, res) => {
   }
 };
 
-export const remove = (req, res) => res.send("remove");
+export const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { password } = req.body;
+    const isExist = await Comment.find({ _id: id });
+
+    if (isExist.length) {
+      const oldPassword = isExist[0]["password"];
+      const isPasswordCorrect = await bcrypt.compare(password, oldPassword);
+      console.log(isPasswordCorrect);
+      if (isPasswordCorrect) {
+        await Comment.deleteOne({ _id: id });
+        res.json({ result: 1, msg: "글이 성공적으로 삭제되었습니다." });
+      } else {
+        res.json({ result: 0, msg: "비밀번호가 틀렸습니다." });
+      }
+    } else {
+      res.json({ result: 0, msg: "해당 글이 존재하지 않습니다." });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({ result: 0, msg: "알수없는 에러가 발생했습니다." });
+  }
+};
